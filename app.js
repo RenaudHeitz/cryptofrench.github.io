73c89fb3dd5cf4f8dab707d4eb63df40724921b1
heitz renaud <renaudheitz@hotmail.fr>
2018-03-29 16:19:19 +0700

    Modify port

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.post('/', function (req, res) {
                             
 
 
-app.listen(5000, function () {
-           console.log('Example app listening on port 5000!')
-           })
+app.listen(process.env.PORT || 3000);
+
 
